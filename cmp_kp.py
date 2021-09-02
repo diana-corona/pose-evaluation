dfd5935e7ea486fc4aec1d1b0fca4a86c04989ef
Diana Corona <45345139+diana-corona@users.noreply.github.com>
2021-09-02 23:55:57 +0100

    fix: assert df1.shape == df2.shape

--- cmp_kp.py
+++ cmp_kp.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 from argparse import ArgumentParser
@@ -9,25 +10,29 @@ df1, df2 = parser.parse_args().input
 
 df1 = pd.read_pickle(df1)
 df2 = pd.read_pickle(df2)
+df1 = df1.dropna()
+df2 = df2.dropna()
 
 df1 = df1.sort_values(by=['file_name', 'frame_number'])
 #df2 = df1.sort_values(by=['file_name', 'frame_number'], ascending=False)
 df2 = df2.sort_values(by=['file_name', 'frame_number'])
-
-#print (df1.shape, df2.shape)
-
-
-
-#assert df1.shape == df2.shape
-
-scores = []
+df1_dic = {} 
+df2_dic = {} 
 
 for i in range(df1.shape[0]):
-    file_name1 = df1['file_name'].iloc[i].split('.')[0]
-    file_name2 = df2['file_name'].iloc[i].split('.')[0]
-    assert file_name1 == file_name2
-    assert df1['frame_number'].iloc[i] == df2['frame_number'].iloc[i]
-    if df2['value'].iloc[i] is not None: 
-        scores.append(np.mean(np.abs(df1['value'].iloc[i] - df2['value'].iloc[i]).astype(float)))
+  file_name = str(df1['file_name'].iloc[i].split('.')[0])+str(df1['frame_number'].iloc[i])
+  df1_dic[file_name] = df1['value'].iloc[i]
+
+for i in range(df2.shape[0]):
+  file_name = str(df2['file_name'].iloc[i].split('.')[0])+str(df2['frame_number'].iloc[i])
+  df2_dic[file_name] = df2['value'].iloc[i]
+
+scores = []
+for key in df1_dic.keys():
+  if key in df2_dic.keys():
+    scores.append(np.mean(np.abs(df1_dic[key] - df2_dic[key]).astype(float)))
+  else:
+    print(key)
+#assert df1.shape == df2.shape
 
 print ("Average difference: %s" % np.mean(scores))
